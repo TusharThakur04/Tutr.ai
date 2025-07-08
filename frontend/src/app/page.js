d176f257740c462ec333ebcb2ce9d13a419b7e77
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-07-08 20:10:15 +0530

    refactor:homepage

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,23 +1,10 @@
 import Advantages from "@/components/advantages/Advantages";
-import { Button } from "@/components/common/button";
-import { Card, CardContent } from "@/components/common/card";
 import CTA from "@/components/cta/CTA";
 import Example from "@/components/example/Example";
 import Features from "@/components/features/Features";
 import Footer from "@/components/footer/Footer";
 import Hero from "@/components/hero/Hero";
 import Navigation from "@/components/navigation/Navigation";
-import {
-  Upload,
-  MessageSquare,
-  CheckCircle,
-  Search,
-  FileText,
-  Zap,
-  Shield,
-  Github,
-  Mail,
-} from "lucide-react";
 
 const page = () => {
   return (
