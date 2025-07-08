7dd70ef9d44d014e73268f7ca59fffb882af5f12
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-07-09 01:09:29 +0530

    fix:signup page initial state

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -10,7 +10,7 @@ import Navigation from "@/components/navigation/Navigation";
 import { useState } from "react";
 
 const page = () => {
-  const [loggingState, setLoggingState] = useState(true);
+  const [loggingState, setLoggingState] = useState(false);
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-blue-50">
       <Navigation setLoggingState={setLoggingState} />
