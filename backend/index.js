0a37924cda65d426217a055d91eaa97eecccbd56
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-07-09 15:05:56 +0530

    feat:set up express boilerplate

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
