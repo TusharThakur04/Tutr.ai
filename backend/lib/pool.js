cdc20535cf0a94b7ac268884853f30c271839660
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-07-28 01:07:11 +0530

    chore:setup database connection

---
+++ backend/lib/pool.js
@@ -0,0 +1,9 @@
+// db.js
+import pg from "pg";
+
+export const pgPool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
