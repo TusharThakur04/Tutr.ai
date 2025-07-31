b583d8eaadf2b1a01f90303b53779b27cf95b98a
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-08-01 02:32:04 +0530

    feat: implented context retrieval from uploaded files based on user query

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,12 +1,31 @@
+import { pgPool } from "../lib/pool.js";
 import embeddingChunks from "../utils/embeddings.js";
 
 const chatController = async (req, res) => {
   try {
     const { message } = req.body;
 
+    // generating embeddings for the messages
     const embedding = await embeddingChunks(message);
+    const vectorString = JSON.stringify(embedding);
+
+    console.log("retriving context");
+    const result = await pgPool.query(
+      `SELECT *, embedding <=> $1 AS similarity
+     FROM chunks
+     ORDER BY embedding <=> $1
+     LIMIT 5`,
+      [vectorString]
+    );
+    const similarChunks = result.rows;
+    const context = similarChunks.map((chunk) => chunk.content).join("\n\n");
+    console.log("context:", context);
+
     console.log(message, embedding);
-  } catch (err) {}
+  } catch (err) {
+    console.error("Error in chatController:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export default chatController;
